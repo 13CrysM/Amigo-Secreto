946cf04886da66be088f89315d24ebb46a77781c
13CrysM <crysmupe@gamil.com>
2025-01-26 20:23:14 -0600

    Agregamos funcion de sorteo

--- app.js
+++ app.js
@@ -38,8 +38,36 @@ function enlistarAmigos () {
 
 // Realiza el sorteo de los nombres para crear los pares de amigos secretos.
 function sortearAmigo(){
+    //Verificamos que la lista tenga por lo menos dos elementos
+    if (lista.length < 2) {
+        alert('Debes de agregar por lo menos dos nombres.');
+        return;
+    }
+    let resultado = document.getElementById ('resultado');
+    resultado.innerHTML = '';
+    let copiaLista; 
+    // Ciclo para evitar que se empareje con la misma persona. 
+    do {
+        //creamos una copia de la lista para no alterar la original.
+        copiaLista = [...lista];
+        copiaLista.sort(() => Math.random() - 0.5);
+        console.log("copia lista: " + copiaLista)
+    } while (copiaLista.some((amigo, index) => amigo === lista[index]));
+    
+    // Ciclo para crear los emparejamientos
+    for (let i = 0; i < lista.length; i++) {
+        let receptor;
+
+        // Asegurarse de que el receptor no sea el mismo que el emisor
+        do {
+            receptor = copiaLista[(i + 1) % copiaLista.length];
+        } while (receptor === lista[i]); // Asegurarse de que el receptor no sea el mismo que el emisor
+
+        // Asignacion de amigos secretos
+        let li = document.createElement('li');
+        li.textContent = `${lista[i]} -> ${receptor}`;
+        resultado.appendChild(li);
+    }
 
     console.log("Sorteando amigo")
 }
-
-//document.getElementById('agregar').addEventListener('click', agregarAmigo);
